11debed7b06b2a4e01c7789ea3571a85773c97a5
Matias <matias.sanchez.0097@gmail.com>
2026-08-08 14:07:41 -0300

    hide whatsapp link

--- src/data/social-links.ts
+++ src/data/social-links.ts
@@ -1,5 +1,5 @@
 import { FiGithub } from "react-icons/fi";
-import { FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 export const socialLinks = {
   GitHub: {
@@ -9,9 +9,5 @@ export const socialLinks = {
   LinkedIn: {
     href: "https://www.linkedin.com/in/matias-sanchez-454152182/",
     icon: FaLinkedinIn,
-  },
-  Whatsapp: {
-    href: "https://wa.me/1158678526",
-    icon: FaWhatsapp,
   },
 };
